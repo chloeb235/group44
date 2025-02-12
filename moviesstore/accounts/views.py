c116c7094e6e1fb515fc3911fb83dcdabd476797
Anoushka Gandotra <anoushka107@gmail.com>
2025-02-11 19:15:01 -0500

    further password reset functionality

--- moviesstore/accounts/views.py
+++ moviesstore/accounts/views.py
@@ -50,8 +50,44 @@ def signup(request):
                 return render(request, 'accounts/signup.html',
                     {'template_data': template_data})
 
+
 def password_reset(request):
+    template_data = {}
+    template_data['title'] = 'Password Reset'
+
     if request.method == "POST":
-        email = request.POST.get("email")
-        print("Password reset requested for:", email)
-    return render(request, "accounts/password_reset.html")
+        if "email" in request.POST and "new_password" not in request.POST:
+
+            email = request.POST.get("email")
+            user = User.objects.filter(email=email).first()
+
+            if user:
+                request.session["reset_email"] = email
+                template_data["message"] = "We recognize you as a registered user!"
+                template_data["email"] = email
+            else:
+                template_data["error"] = "This email is not registered!"
+
+        elif "new_password" in request.POST and "confirm_password" in request.POST:
+
+            email = request.session.get("reset_email")
+            user = User.objects.filter(email=email).first()
+
+            if user:
+                new_password = request.POST["new_password"]
+                confirm_password = request.POST["confirm_password"]
+
+                if new_password == confirm_password:
+                    user.set_password(new_password)
+                    user.save()
+                    del request.session["reset_email"]
+
+
+                    template_data["success"] = True
+                    template_data["message"] = "Your password was successfully updated!"
+                else:
+                    template_data["error"] = "Passwords do not match!"
+            else:
+                template_data["error"] = "Session expired. Please try again."
+
+    return render(request, "accounts/password_reset.html", {"template_data": template_data})
